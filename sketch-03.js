cbb12c426dd0258fe9d4b71933bf50941d386e62
Mateus Guerra <mateusguerra@MacBook-Pro-de-Mateus.local>
2021-12-04 22:57:37 +0100

    my version

--- sketch-03.js
+++ sketch-03.js
@@ -9,7 +9,7 @@ const settings = {
 
 const sketch = ({ context, width, height }) => {
   const agents = [];
-  for (let i = 0; i < 120; i++) {
+  for (let i = 0; i < 800; i++) {
     const x = random.range(0, width);
     const y = random.range(0, height );
     agents.push(new Agent(x, y))
@@ -23,11 +23,12 @@ const sketch = ({ context, width, height }) => {
       for (let j = i + 1; j < agents.length; j++) {
         const other = agents[j]; 
         const dist = agent.pos.getDistance(other.pos);
-        if(dist < 200) {
-          context.lineWidth = math.mapRange(dist, 0, 200, 6, 1);
+        if(dist < 120) {
+          context.lineWidth = math.mapRange(dist, 0, 120, 1.5, .0001);
           context.beginPath();
           context.moveTo(agent.pos.x, agent.pos.y);
           context.lineTo(other.pos.x, other.pos.y);
+          context.strokeStyle = '#999';
           context.stroke();
         }
       }
@@ -60,7 +61,7 @@ class Agent {
   constructor(x, y) {
     this.pos = new Vector(x, y);
     this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
-    this.radius = random.range(2, 8);
+    this.radius = random.range(1, 3);
   }
 
   bounce(width, height) {
